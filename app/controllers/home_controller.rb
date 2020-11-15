7d7af7c652bafcc22748360f600f7893237a8ec9
mckay <simi7236@gmail.com>
2020-11-16 03:00:54 +0900

    paging over, under flow redirect

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,11 +13,17 @@ class HomeController < ApplicationController
                     # params[:current_page].to_i * 5
                     Integer(params[:current_page]) * 5
                   end
-    print(params[:current_page])
-    print(start_index: start_index)
-    # 첫 offset 개를 빼고 5개 -> (0,1 빼고 2,3,4,5,6)
-    @posts = Post.limit(5).offset(start_index)
 
     @posts_count = Post.count
+
+    unless params[:current_page].nil?
+      if Integer(params[:current_page]).negative?
+        redirect_to '/'
+      elsif Integer(params[:current_page]) > @posts_count / 5
+        redirect_to "/#{@posts_count / 5}"
+      end
+    end
+    # 첫 offset 개를 빼고 5개 -> (0,1 빼고 2,3,4,5,6)
+    @posts = Post.limit(5).offset(start_index)
   end
 end
