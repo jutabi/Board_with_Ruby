92438c6d0a4e7c8d3af934ad92a46c4290817303
mckay <simi7236@gmail.com>
2020-11-16 02:30:31 +0900

    paging button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,23 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
   def index
-    @posts = Post.all
+    # @posts = Post.all.reverse_order
+
+    # 첫 5개 -> (0,1,2,3,4)
+    # @posts = Post.limit(5).reverse_order
+
+    start_index = if params[:current_page].nil?
+                    0
+                  else
+                    # params[:current_page].to_i * 5
+                    Integer(params[:current_page]) * 5
+                  end
+    print(params[:current_page])
+    print(start_index: start_index)
+    # 첫 offset 개를 빼고 5개 -> (0,1 빼고 2,3,4,5,6)
+    @posts = Post.limit(5).offset(start_index)
+
+    @posts_count = Post.count
   end
 end
